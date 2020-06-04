a59615107d06ff765baf09af095d2a0d513d3feb
hemantjava <hemantjava90@gmail.com>
2020-06-04 23:22:21 +0530

    Initial commit

---
+++ src/main/java/com/hemant/beanvalidations/exception/CustomExceptionHandler.java
@@ -0,0 +1,51 @@
+package com.hemant.beanvalidations.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class CustomExceptionHandler
+{
+
+    @ExceptionHandler(InvalidInputException.class)
+    public final ResponseEntity<ErrorResponse> handleUserNotFoundException(InvalidInputException ex)
+    {
+        ErrorResponse error = new ErrorResponse(HttpStatus.LENGTH_REQUIRED.value(),ex.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<ErrorResponse>(error, HttpStatus.LENGTH_REQUIRED);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        return errors;
+    }
+    @ExceptionHandler(Exception.class) //Global exception handling
+    public final ResponseEntity<ErrorResponse> handleUnAuthenticException(Exception ex)
+    {
+        ErrorResponse error = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),ex.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<ErrorResponse>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class) //Global exception handling
+    public final ResponseEntity<ErrorResponse> handleUnAuthenticException(HttpMediaTypeNotAcceptableException ex)
+    {
+        ErrorResponse error = new ErrorResponse(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value(),ex.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<ErrorResponse>(error, HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+    }
+}
